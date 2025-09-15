15540ee5cbcf6fd1c0f63585d7da685470eb9a1d
Khoroshilovdg <160841554+Khoroshilovdg@users.noreply.github.com>
2025-09-15 13:54:42 +0300

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
@@ -8,4 +9,10 @@ export default defineConfig({
     mode: 'standalone',
   }),
   integrations: [tailwind()],
+  vite: {
+    define: {
+      'import.meta.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+      'import.meta.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
+    }
+  }
 });
