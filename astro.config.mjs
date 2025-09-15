ab682a8314f967b93144c512c1474f06a1b1775b
Khoroshilovdg <160841554+Khoroshilovdg@users.noreply.github.com>
2025-09-15 20:08:39 +0300

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
   integrations: [tailwind()],
   vite: {
     define: {
